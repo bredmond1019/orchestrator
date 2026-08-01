a8b6959f05d530ce6e74755bf6eea0cebf627532
Brandon Redmond <bredmond1019@gmail.com>
2026-08-01 18:13:20 -0300

    fix: fix pass 1 for or-x2-sdlc-evals-retirement-task1

--- app/workflows/workflow_registry.py
+++ app/workflows/workflow_registry.py
@@ -4,12 +4,10 @@ from workflows.document_ingest_workflow import DocumentIngestWorkflow
 from workflows.document_qa_workflow import DocumentQAWorkflow
 from workflows.memory_consolidation_workflow import MemoryConsolidationWorkflow
 from workflows.memory_ingest_workflow import MemoryIngestWorkflow
-from workflows.sdlc_flow_workflow import SDLCFlowWorkflow
 
 
 class WorkflowRegistry(Enum):
     DOCUMENT_INGEST = DocumentIngestWorkflow
     DOCUMENT_QA = DocumentQAWorkflow
-    SDLC_FLOW = SDLCFlowWorkflow
     MEMORY_INGEST = MemoryIngestWorkflow
     MEMORY_CONSOLIDATION = MemoryConsolidationWorkflow
